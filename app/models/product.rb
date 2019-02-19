05da1112a6963b6486c1778fc50d94cb3a6ae0df
acv <anhnt@atmarkcafe.org>
2019-02-19 15:02:42 +0700

    Add scope for model query

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
-  validates :title, :description, presence: true 
-  validates :price, numericality: {greater_than: 0}
+  scope :published, -> { where(published: true) }
+  scope :priced_more_than, ->(price) { where('price > ?', price) }
+  scope :priced_more_than_index, -> (price) { where('price > ?', price).select(:price, :index) }
+  validates :title, :description, presence: true
+  validates :price, numericality:{ greater_than: 0 }
   validate :title_is_shorter_than_description
 
   def title_is_shorter_than_description
