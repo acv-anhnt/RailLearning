17ff4ccddfdbf2be9912c06d5968eecd1610990f
acv <anhnt@atmarkcafe.org>
2019-02-20 17:53:04 +0700

    Add show view for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.includes(:category).published
   end
+  def show
+    @product = Product.includes(:category).find(params[:id])
+  end
 end
