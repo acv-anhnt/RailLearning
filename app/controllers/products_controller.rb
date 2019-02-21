415b3d4aace6037f4d509f8d814cf1129a12c1af
acv <anhnt@atmarkcafe.org>
2019-02-21 16:13:01 +0700

    Add update for product list

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,15 @@ class ProductsController < ApplicationController
   def index
     @products = Product.includes(:category).published
   end
+
   def show
     @product = Product.includes(:category).find(params[:id])
   end
+
   def new
     @product = Product.new
   end
+
   def create
     product_params = params.require(:product).permit(:title, :description, :price, :published, :category_id)
     @product = Product.new(product_params)
@@ -16,6 +19,23 @@ class ProductsController < ApplicationController
       redirect_to products_path
     else
       flash.now[:notice] = 'There is an error in your form'
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :new
+  end
+
+  def update
+    product_params = params.require(:product).permit(:title, :description, :price, :published, :category_id)
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = 'You have successfully updated the product'
+      redirect_to product_path(@product)
+    else
+      flash.now[:notice] = 'There is an error when updating'
       render :new
     end
   end
